1ebd17a8c736abd4ca6b31ec0501fd03abe3ab98
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-02-16 20:07:07 +0530

    The project is created

---
+++ BitwiseOperator/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String [] args){
+        // operands --> byte short int long
+        // and operator
+        // or operator
+        // xor ^
+        // not ~
+        // left shift <<
+        // right shift >>
+        // unsigned right shift >>>
+        int a = 5;
+    }
+}
