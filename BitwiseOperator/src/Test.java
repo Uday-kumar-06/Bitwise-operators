0be66ea0b8b5aaa6dfce49f89da5729d858f6039
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-02-17 13:47:44 +0530

    Added some code

--- BitwiseOperator/src/Test.java
+++ BitwiseOperator/src/Test.java
@@ -1,13 +1,39 @@
 public class Test {
     public static void main(String [] args){
         // operands --> byte short int long
-        // and operator
-        // or operator
+        // and operator &
+        // or operator |
         // xor ^
         // not ~
         // left shift <<
         // right shift >>
         // unsigned right shift >>>
         int a = 5;
+        int b = 5 & 4;
+        // for & operator we get binary 1 if both bits are 1
+        System.out.println(b);
+
+
+        int c = 5 | 4;
+        // for | operator we get binary 1, if either of the both bits are 1
+        System.out.println(c);
+
+        int d = 5 ^ 4;
+        // for ^ operator we get binary 1, if both bits are not same
+        System.out.println(d);
+
+        int e = ~a;
+        // for ~ operator we change 1 to 0 and 0 to 1
+        System.out.println(e);
+
+        int f = a << 1;
+        System.out.println("5 -->" + Integer.toBinaryString(a));
+        System.out.println(f);
+        System.out.println("{\f} -->" + Integer.toBinaryString(f));
+
+        int g = a >> 1;
+        System.out.println("5 -->" + Integer.toBinaryString(a));
+        System.out.println(f);
+        System.out.println("{\f} -->" + Integer.toBinaryString(g));
     }
 }
